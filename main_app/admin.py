91fc56fb99171e6d1aecabdc3b5b04479367396f
Ochouno2364 <ochouno2364@gmail.com>
2025-06-15 16:51:05 -0400

    one to many

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, Location
 
 # Register your models here.
 
 admin.site.register(Trip)
+admin.site.register(Location)
--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,31 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+BODIES = (
+    ('O', 'Ocean'),
+    ('L', 'Lake'),
+    ('R', 'River'),
+    ('P', 'Pond'),
+    ('C', 'Creek'),
+)
+WATERS = (
+    ('F', 'Fresh Water'),
+    ('S', 'Salt Water'),
+
+)
+
+WEATHER = (
+    ('S', 'Sunny'),
+    ('C', 'Cloudy'),
+    ('R', 'Rainy'),
+    ('T', 'Thunder'),
+    ('W', 'Windy'),
+    ('F', 'Foggy'),
+)
 
 class Trip(models.Model):
     name = models.CharField(max_length=50)
-    date = models.DateField()
+    date = models.DateField('Date Of Trip')
     gear = models.CharField(max_length=100)
     # fish = models.ManyToManyField()
 
@@ -15,3 +36,14 @@ class Trip(models.Model):
     def get_absolute_url(self):
         return reverse('trip-detail', kwargs={'trip_id': self.id})
 
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    body = models.CharField(max_length=1, default=[0][0], choices=BODIES)
+    water = models.CharField(max_length=1, default=[0][0], choices=WATERS)
+    address = models.CharField(max_length=200)
+    weather = models.CharField(max_length=20, default=[0][0], choices=WEATHER)
+
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, default=[0][0])
+
+    def __str__(self):
+        return f"{self.name} {self.get_body_display()} at {self.address}"
